04c2c22e119095e5538578be8bf0566efcba99ec
ahmed-nossier <32619363+ahmed-nossier@users.noreply.github.com>
2020-09-15 03:54:24 +0300

    add sounds click and camera- scrolltop button -slow scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,105 @@
 (function ($) {
-    "use strict"; // Start of use strict
+  "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-            this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length ?
-                target :
-                $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate({
-                        scrollTop: target.offset().top - 70,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
-            }
-        }
-    });
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 70,
+          },
+          1000,
+          "easeInOutExpo"
+        );
+        return false;
+      }
+    }
+  });
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
-    });
+  // Closes responsive menu when a scroll trigger link is clicked
 
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#mainNav",
-        offset: 100,
-    });
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 100,
+  });
 
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
+  // Collapse Navbar
+  var navbarCollapse = function () {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
+
+/*****************/
+
+var playSound = new Audio();
+
+var playSoundcam = new Audio();
+playSound.src = "./assets/audio/click.mp3";
+playSoundcam.src = "./assets/audio/cam.mp3";
+$(".bateekh").click(function () {
+  playSound.play();
+});
+function enableLoop() {
+  playSound.loop = true;
+}
+
+/************************* */
+function isOnScreen(elem) {
+  if (elem.length == 0) {
+    return;
+  }
+  var $window = jQuery(window);
+  var viewport_top = $window.scrollTop();
+  var viewport_height = $window.height();
+  var viewport_bottom = viewport_top + viewport_height;
+  var $elem = jQuery(elem);
+  var top = $elem.offset().top;
+  var height = $elem.height();
+  var bottom = top + height;
+  return top >= viewport_top && top < viewport_bottom;
+}
+jQuery(document).ready(function () {
+  window.addEventListener("scroll", function (e) {
+    if (isOnScreen(jQuery(".Home"))) {
+      playSoundcam.play();
+    }
+  });
+});
+
+/*********scroll top button ************ */
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
